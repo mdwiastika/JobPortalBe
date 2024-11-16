1b72c5a2edef3a2faa88c7930e142032114dadc3
Marcel Dwi Astika <marceldwias@gmail.com>
2024-11-16 15:45:37 +0700

    feat: add authentication api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'full_name' => 'required|max:55',
+                'email' => 'email|required|unique:users',
+                'password' => 'required|confirmed',
+                'role' => 'required',
+            ]);
+            $validatedData['password'] = bcrypt($request->password);
+            $user = User::create($validatedData);
+            $user->assignRole($request->role);
+            $token = $user->createToken('auth_token')->plainTextToken;
+            $user['token'] = $token;
+            return new UserResource('success', 'Successfully Register User!', $user);
+        } catch (\Throwable $th) {
+            return new UserResource('error', $th->getMessage(), null);
+        } catch (ValidationException $ve) {
+            return new UserResource('error', $ve->errors(), null);
+        }
+    }
+    public function login(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'email' => 'email|required',
+                'password' => 'required',
+            ]);
+            if (!Auth::attempt($validatedData)) {
+                return new UserResource('error', 'Unauthorized', null);
+            }
+            $user = User::where('email', $validatedData['email'])->firstOrFail();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            $user['token'] = $token;
+            return new UserResource('success', 'Successfully Login User!', $user);
+        } catch (\Throwable $th) {
+            return new UserResource('error', $th->getMessage(), null);
+        } catch (ValidationException $ve) {
+            return new UserResource('error', $ve->errors(), null);
+        }
+    }
+    public function logout(Request $request)
+    {
+        try {
+            $request->user()->currentAccessToken()->delete();
+            return new UserResource('success', 'Successfully Logout User!', null);
+        } catch (\Throwable $th) {
+            return new UserResource('error', $th->getMessage(), null);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('users', UserController::class);
+    Route::prefix('auth')->group(function () {
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    });
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user()->load('roles');
+        });
+        Route::apiResource('users', UserController::class)->middleware('role:super_admin|admin');
+    });
 });
