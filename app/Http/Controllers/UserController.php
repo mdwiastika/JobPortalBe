0fc05b110fb5547e338fb54336740eae518b06d1
Marcel Dwi Astika <marceldwias@gmail.com>
2024-11-16 13:52:50 +0700

    feat: RUD User

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Mockery\Matcher\Not;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        try {
+            $users = User::whereDoesntHave('roles', function ($query) {
+                $query->where('name', 'super_admin');
+            })->with(['roles'])->get();
+            return new UserResource('success', 'Data fetched successfully', $users);
+        } catch (\Throwable $th) {
+            return new UserResource('error', $th->getMessage(), null);
+        }
+    }
+    public function update(Request $request, $id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $validatedData = $request->validate([
+                'full_name' => 'required',
+                'email' => 'required|email|unique:users,email,' . $id,
+                'password' => 'nullable|confirmed|min:8',
+                'roles' => 'required',
+            ]);
+            $user->update($validatedData);
+            $user->syncRoles($validatedData['roles']);
+            return new UserResource('success', 'Data updated successfully', $user);
+        } catch (ValidationException $ve) {
+            return new UserResource('error', $ve->errors(), null);
+        } catch (\Throwable $th) {
+            return new UserResource('error', $th->getMessage(), null);
+        } catch (ModelNotFoundException $th) {
+            return new UserResource('error', $th->getMessage(), null);
+        }
+    }
+    public function destroy($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+            return new UserResource('success', 'Data deleted successfully', null);
+        } catch (\Throwable $th) {
+            return new UserResource('error', $th->getMessage(), null);
+        } catch (ModelNotFoundException $th) {
+            return new UserResource('error', $th->getMessage(), null);
+        }
+    }
+}
